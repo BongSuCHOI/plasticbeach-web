2f768acdda240eeeb565e2e0d3413dae3fb357f3
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2021-12-10 17:18:12 +0900

    refactor script

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// module
+// module import
 import customCursor from "./module/customCursor.js";
 import circleLogoAnimation from "./module/CircleLogo.js";
 import Marquee from "./module/Marquee.js";
@@ -6,35 +6,32 @@ import MouseOverTooltip from "./module/MouseOverTooltip.js";
 import textJson from "./data/text.json";
 import workJson from "./data/work.json";
 
-// css
+// css import
 import commonCSS from "../css/scss/common.scss";
 import styleCSS from "../css/scss/style.scss";
 import contactCSS from "../css/scss/contact.scss";
 import workCSS from "../css/scss/work.scss";
 
-// draw DOM & convert lang
-class DrawDomAndConvert {
+// DOM text bind
+class DomTextBind {
     constructor() {
         this.lang = "en";
-        this.textData;
         this.workData;
+        this.textData;
     }
 
-    init() {
-        this.workGetJson();
-        this.textGetJson();
-    }
+    async getJson() {
+        const workRes = fetch(workJson).then((workRes) => workRes.json());
+        await workRes.then((workData) => (this.workData = workData));
 
-    async workGetJson() {
-        const response = fetch(workJson).then((response) => response.json());
-        await response.then((workData) => (this.workData = workData));
+        const textRes = fetch(textJson).then((textRes) => textRes.json());
+        await textRes.then((textData) => (this.textData = textData));
+        
         this.bindWorkList();
-    }
-
-    async textGetJson() {
-        const response = fetch(textJson).then((response) => response.json());
-        await response.then((textData) => (this.textData = textData));
         this.bindText();
+
+        // children class
+        convert.parentData(this.workData, this.textData)
     }
 
     bindWorkList() {
@@ -52,11 +49,11 @@ class DrawDomAndConvert {
                     class="video_tooltip"
                     width="560"
                     height="315"
-                    src="https://www.youtube.com/embed/${data[i].url}?&mute=1&enablejsapi=1&playsinline=1&color=white"
-                    frameborder="0"allowfullscreen
-                    >
+                    src="https://www.youtube.com/embed/${data[i].url}?&mute=1&enablejsapi=1&playsinline=1&color=white&controls=0"
+                    frameborder="0"
+                    allowfullscreen>
                 </iframe>
-                `;
+            `;
             parent.appendChild(html);
         }
 
@@ -71,48 +68,73 @@ class DrawDomAndConvert {
         const timelineArr = Object.entries(data[2]);
         const categoryArr = Object.entries(data[3]);
 
-        // 일반 content text
-        for (let i = 0; i < textArr.length; i++) {
-            const target = document.querySelector(`[data-name='${textArr[i][0]}']`);
-            target.innerHTML = textArr[i][1].en;
+        function bindFuc(variable, name) {
+            // 일반 content text 및 category select radio btn text
+            if (name == 'content' || name == 'category') {
+                for (let i = 0; i < variable.length; i++) {
+                    const target = document.querySelector(`[data-name='${variable[i][0]}']`);
+                    target.innerHTML = variable[i][1].en;
 
-            // marquee text
-            if (target.classList.contains("marquee_text")) {
-                target.innerHTML = textArr[i][1].en + textArr[i][1].en;
+                    // 일반 content text
+                    if (name == 'content') {
+                        // marquee text
+                        if (target.classList.contains("marquee_text")) {
+                            target.innerHTML = variable[i][1].en + variable[i][1].en;
+                        }
+                        // contact input placeholder
+                        if (target.hasAttribute('placeholder')) {
+                            target.innerHTML = '';
+                            target.setAttribute('placeholder', variable[i][1].en)
+                        }
+                    }
+                }
             }
-
-            // contact input placeholder
-            if (target.hasAttribute('placeholder')) {
-                target.innerHTML = '';
-                target.setAttribute('placeholder', textArr[i][1].en)
+            // 예산/일정 select box option text
+            else if (name == 'budgiet' || name == 'timeline') {
+                for (let i = 0; i < variable.length; i++) {
+                    const parent = document.querySelector(`select.${name}`);
+                    const html = document.createElement("option");
+                    html.setAttribute("class", variable[i][0]);
+                    html.innerHTML = variable[i][1].en;
+                    parent.appendChild(html);
+                }
             }
         }
-
-        // 예산 select box option text
-        for (let i = 0; i < budgietArr.length; i++) {
-            const parent = document.querySelector("select.budgiet");
-            const html = document.createElement("option");
-            html.setAttribute("class", budgietArr[i][0]);
-            html.innerHTML = budgietArr[i][1].en;
-            parent.appendChild(html);
-        }
-
-        // 일정 select box text
-        for (let i = 0; i < timelineArr.length; i++) {
-            const parent = document.querySelector("select.timeline");
-            const html = document.createElement("option");
-            html.setAttribute("class", timelineArr[i][0]);
-            html.innerHTML = timelineArr[i][1].en;
-            parent.appendChild(html);
-        }
-
-        // category select radio btn text
-        for (let i = 0; i < categoryArr.length; i++) {
-            const target = document.querySelector(`[data-name='${categoryArr[i][0]}']`);
-            target.innerHTML = categoryArr[i][1].en;
-        }
-
+        
+        bindFuc(textArr, 'content');
+        bindFuc(budgietArr, 'budgiet');
+        bindFuc(timelineArr, 'timeline');
+        bindFuc(categoryArr, 'category');
         Marquee(".marquee", 0.2);
+    }
+
+    workCategory() {
+        const list = document.querySelectorAll('.work_list .list');
+
+        for (let i = 0; i < list.length; i++) {
+            let listCategory = list[i].getAttribute('category');
+            if (this.id == 'all') {
+                list[i].style.display = 'flex'
+            } else if (listCategory == this.id) {
+                list[i].style.display = 'flex'
+            } else {
+                list[i].style.display = 'none'
+            }
+        }
+    }
+}
+
+// convert color & text
+class Convert extends DomTextBind {
+    constructor(lang) {
+        super(lang)
+        this.workData;
+        this.textData;
+    }
+
+    parentData(work, text) {
+        this.workData = work;
+        this.textData = text;
     }
 
     changeWork() {
@@ -135,71 +157,66 @@ class DrawDomAndConvert {
         const budgietArr = Object.entries(data[1]);
         const timelineArr = Object.entries(data[2]);
         const categoryArr = Object.entries(data[3]);
+        let lang = this.lang;
 
-        for (let i = 0; i < textArr.length; i++) {
-            const target = document.querySelector(`[data-name='${textArr[i][0]}']`);
-
-            // 일반 content text
-            if (this.lang === "en") {
-                target.innerHTML = textArr[i][1].en;
-
-                // marquee text
-                if (target.classList.contains("marquee_text")) {
-                    target.innerHTML = textArr[i][1].en + textArr[i][1].en;
+        function changeFuc(variable, name) {
+            // 일반 content text 및 category select radio btn text
+            if (name == 'content' || name == 'category') {
+                for (let i = 0; i < variable.length; i++) {
+                    const target = document.querySelector(`[data-name='${variable[i][0]}']`);
+                    
+                    if (lang === "en") {
+                        target.innerHTML = variable[i][1].en;
+                        // 일반 content text
+                        if(name == 'content') {
+                            // marquee text
+                            if (target.classList.contains("marquee_text")) {
+                                target.innerHTML = textArr[i][1].en + textArr[i][1].en;
+                            }
+                            // contact input placeholder
+                            if (target.hasAttribute('placeholder')) {
+                                target.setAttribute('placeholder', textArr[i][1].en)
+                            }
+                        }
+                    } else {
+                        target.innerHTML = variable[i][1].ko;
+                        // 일반 content text
+                        if(name == 'content') {
+                            // marquee text
+                            if (target.classList.contains("marquee_text")) {
+                                target.innerHTML = textArr[i][1].ko + textArr[i][1].ko;
+                            }
+                            // contact input placeholder
+                            if (target.hasAttribute('placeholder')) {
+                                target.setAttribute('placeholder', textArr[i][1].ko)
+                            }
+                        }
+                    }
                 }
-
-                // contact input placeholder
-                if (target.hasAttribute('placeholder')) {
-                    target.setAttribute('placeholder', textArr[i][1].en)
-                }
-            } else {
-                target.innerHTML = textArr[i][1].ko;
-
-                // marquee text
-                if (target.classList.contains("marquee_text")) {
-                    target.innerHTML = textArr[i][1].ko + textArr[i][1].ko;
-                }
-
-                // contact input placeholder
-                if (target.hasAttribute('placeholder')) {
-                    target.setAttribute('placeholder', textArr[i][1].ko)
+            }
+            // 예산/일정 select box option text
+            else if (name == 'budgiet' || name == 'timeline') {
+                for (let i = 0; i < variable.length; i++) {
+                    const target = document.querySelector(`.${variable[i][0]}`);
+                    
+                    if (lang === "en") {
+                        target.innerHTML = variable[i][1].en;
+                    } else {
+                        target.innerHTML = variable[i][1].ko;
+                    }
                 }
             }
         }
 
-        // 예산 select box option text
-        for (let i = 0; i < budgietArr.length; i++) {
-            const target = document.querySelector(`.${budgietArr[i][0]}`);
-            if (this.lang === "en") {
-                target.innerHTML = budgietArr[i][1].en;
-            } else {
-                target.innerHTML = budgietArr[i][1].ko;
-            }
-        }
-
-        // 일정 select box text
-        for (let i = 0; i < timelineArr.length; i++) {
-            const target = document.querySelector(`.${timelineArr[i][0]}`);
-            if (this.lang === "en") {
-                target.innerHTML = timelineArr[i][1].en;
-            } else {
-                target.innerHTML = timelineArr[i][1].ko;
-            }
-        }
-
-        // category select radio btn text
-        for (let i = 0; i < categoryArr.length; i++) {
-            const target = document.querySelector(`[data-name='${categoryArr[i][0]}']`);
-            if (this.lang === "en") {
-                target.innerHTML = categoryArr[i][1].en;
-            } else {
-                target.innerHTML = categoryArr[i][1].ko;
-            }
-        }
+        changeFuc(textArr, 'content');
+        changeFuc(budgietArr, 'budgiet');
+        changeFuc(timelineArr, 'timeline');
+        changeFuc(categoryArr, 'category');
     }
 
     reverseColorTheme() {
         const state = document.documentElement.getAttribute("color-theme");
+
         if (state === "white") {
             document.documentElement.setAttribute("color-theme", "blue");
         } else {
@@ -208,43 +225,43 @@ class DrawDomAndConvert {
     }
 
     change() {
-        DOM.lang === "en" ? (DOM.lang = "ko") : (DOM.lang = "en");
-        DOM.changeWork();
-        DOM.changeText();
-        DOM.reverseColorTheme();
-
         const enFont = document.querySelectorAll(".toggle_font");
-        for (let i = 0; i < enFont.length; i++) {
-            enFont[i].classList.contains("Nefarious") ? enFont[i].classList.remove("Nefarious") : enFont[i].classList.add("Nefarious");
-        }
-    }
 
-    workListCategory() {
-        const list = document.querySelectorAll('.work_list .list');
-        for (let i = 0; i < list.length; i++) {
-            let listCategory = list[i].getAttribute('category');
-            if (this.id == 'all') {
-                list[i].style.display = 'flex'
-            } else if (listCategory == this.id) {
-                list[i].style.display = 'flex'
+        convert.lang === "en" ? (convert.lang = "ko") : (convert.lang = "en");
+        convert.changeWork();
+        convert.changeText();
+        convert.reverseColorTheme();
+
+        for (let i = 0; i < enFont.length; i++) {
+            if (enFont[i].classList.contains("Nefarious")) {
+                enFont[i].classList.remove("Nefarious")
             } else {
-                list[i].style.display = 'none'
+                enFont[i].classList.add("Nefarious")
             }
         }
     }
 }
-let DOM = new DrawDomAndConvert();
-document.querySelector(".reverse_color").addEventListener("click", DOM.change);
+
+// instance
+let domTextBind = new DomTextBind();
+let convert = new Convert();
 
 // run
 window.addEventListener("DOMContentLoaded", () => {
-    DOM.init();
+    domTextBind.getJson();
     customCursor.create();
+    circleLogoAnimation.bindSGV();
 });
-circleLogoAnimation.bindSGV();
+
+// reverse color
+document.querySelector(".reverse_color").addEventListener("click", convert.change);
+
+// workListCategory
+const radios = document.querySelectorAll('[type=radio]');
+radios.forEach(elem => elem.addEventListener('click', domTextBind.workCategory));
 
 // custom input[type="file"](file-name bind)
-const bindFileInput = (function () {
+(function bindFileInput() {
     const elem = document.querySelector("#file");
     const bindTarget = document.querySelector(".file_name");
 
@@ -253,7 +270,3 @@ const bindFileInput = (function () {
         bindTarget.value = value;
     });
 })();
-
-// workListCategory
-const radios = document.querySelectorAll('[type=radio]');
-radios.forEach(elem => elem.addEventListener('click', DOM.workListCategory));
