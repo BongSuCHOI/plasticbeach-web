2d18b6ddfc1a2f764a7d40f525378b2db061854c
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2021-12-10 14:17:44 +0900

    asset 경로 변경 및 webpack optimization setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-
     entry: "./src/js/index.js",
 
     output: {
         path: path.resolve(__dirname, "docs"),
-        filename: "[name].js",
+        filename: "asset/js/[name].js",
     },
 
     plugins: [
@@ -16,9 +17,24 @@ module.exports = {
             template: "./index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({
+            filename: "asset/css/[name].css",
+        }),
     ],
 
-    watch: true,
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            name: "vendor",
+            filename: 'asset/js/vendor.js',
+            chunks: "all",
+        },
+    },
 
     module: {
         rules: [
@@ -29,33 +45,30 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: ["@babel/preset-env"],
-                        plugins: ["@babel/plugin-transform-runtime"]
+                        plugins: ["@babel/plugin-transform-runtime"],
                     },
                 },
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             {
                 test: /\.(jpe?g|png|gif)$/,
                 type: "asset",
                 generator: {
-                    filename: "images/[name][ext]?[hash]",
+                    filename: "asset/img/[name][ext]?[hash]",
                 },
             },
             {
                 test: /\.json$/,
                 type: "asset",
-                generator: {
-                    filename: "json/[name][ext]?[hash]",
-                },
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                test: /\.(woff|woff2|ttf|otf)$/i,
                 type: "asset",
                 generator: {
-                    filename: "fonts/[name][ext]?[hash]",
+                    filename: "asset/font/[name][ext]?[hash]",
                 },
             },
         ],
