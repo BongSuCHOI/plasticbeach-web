518bac49359d5a037713cf85a72af9af08753067
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2021-12-21 16:20:53 +0900

    refactor: iframe tooltip 로드 방식 변경

--- src/js/module/MouseOverTooltip.js
+++ src/js/module/MouseOverTooltip.js
@@ -4,47 +4,45 @@ import platformCheck from './PlatformCheck.js';
 function mouseOverTooltip(embedData) {
     if (!platformCheck()) { return }
     
+    const tooltipBox = document.querySelector(".tooltip_box");
+    const iframe = document.createElement("iframe");
     const hoverBox = document.querySelectorAll(".work_list .list");
+    
+    iframe.setAttribute("class", "video_tooltip");
+    iframe.setAttribute("name", "work");
+    iframe.setAttribute("frameborder", "0");
+    iframe.width = "560";
+    iframe.height = "315";
+    tooltipBox.appendChild(iframe);
+
     hoverBox.forEach((obj, i) => {
-        const iframe = document.createElement("iframe");
-        let tooltip = null;
-
-        iframe.setAttribute("class", "video_tooltip");
-        iframe.setAttribute("name", "work");
-        iframe.setAttribute("frameborder", "0");
-        iframe.src = `https://www.youtube.com/embed/${embedData[i]}?rel=0&mute=1&autoplay=1&controls=0&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&color=white"`;
-        iframe.width = "560";
-        iframe.height = "315";
-
         obj.addEventListener("mouseenter", function() {
-            this.appendChild(iframe);
-            tooltip = this.querySelector(".video_tooltip");
-            gsap.to(tooltip, {
+            gsap.to(tooltipBox, {
                 opacity: 1,
                 display: "block",
                 duration: 0.3,
             })
+            iframe.src = `https://www.youtube.com/embed/${embedData[i]}?rel=0&autoplay=1&mute=1&controls=0&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&color=white"`;
         });
         
         obj.addEventListener("mousemove", (e) => {
             const mouseX = e.clientX;
             const mouseY = e.clientY;
 
-            tooltip.style.left = mouseX + "px";
-            tooltip.style.top = mouseY - tooltip.clientHeight + "px";
+            tooltipBox.style.left = mouseX + "px";
+            tooltipBox.style.top = mouseY - tooltipBox.clientHeight + "px";
 
-            if (tooltip.offsetTop < 0) {
-                tooltip.style.top = mouseY + "px";
+            if (tooltipBox.offsetTop < 0) {
+                tooltipBox.style.top = mouseY + "px";
             }
         });
 
-        obj.addEventListener("mouseleave", function() {
-            gsap.to(tooltip, {
+        obj.parentElement.addEventListener("mouseleave", function() {
+            gsap.to(tooltipBox, {
                 opacity: 0,
                 display: "none",
                 duration: 0.3,
             })
-            tooltip = null;
         });
     });
 }
