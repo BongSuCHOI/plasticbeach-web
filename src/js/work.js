5ff5bf75a72f1d73d9f60e69f6cf288c643e067b
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-01-14 16:53:36 +0900

    fix to bug // refactoring

--- src/js/work.js
+++ src/js/work.js
@@ -12,7 +12,7 @@ import platformCheck from './module/PlatformCheck.js';
 // Prod Data
 const prodData = (data) => {
     bind_detail.init(data);
-    listEvent.overEvent.tooltip(data);
+    listEvent.overEvent(data);
 }
 
 // detail bind
@@ -113,16 +113,17 @@ const bind_detail = {
             observer.unobserve(entry.target);
             ioReStart(observer);
             listEvent.clickEvent();
-        }, { threshold: 0.1 });
+        });
 
         const ioReStart = (intersectionObserver) => {
-            lists.forEach(list => {
-                if (!list.nextSibling && pageNum < Math.ceil(data.length/listLength)) {
-                    intersectionObserver.observe(list)
-                } else if (pageNum >= Math.ceil(data.length/listLength)) {
-                    intersectionObserver.disconnect()
-                }
-            })
+            const currentLoadSwiper = document.querySelectorAll(".swiper");
+            const lastElem = currentLoadSwiper[currentLoadSwiper.length - 1];
+            const currentLastLi = lastElem.parentElement.parentElement;
+            if (pageNum < Math.ceil(data.length/listLength)) {
+                intersectionObserver.observe(currentLastLi)
+            } else if (pageNum >= Math.ceil(data.length/listLength)) {
+                intersectionObserver.disconnect()
+            }
         }
 
         addList();
@@ -155,75 +156,73 @@ const listEvent = {
         return bodyScrollBar;
     },
 
-    overEvent: {
-        tooltip: (data) => {
-            if (!platformCheck()) return;
-
-            const embedUrl = data.map(d => d.url);
-            const tooltipBox = document.querySelector(".tooltip_box");
-            const iframe = document.createElement("iframe");
-            const hoverBox = document.querySelectorAll(".work_list .list button");
-
-            iframe.setAttribute("class", "video_tooltip");
-            iframe.setAttribute("name", "work");
-            iframe.setAttribute("frameborder", "0");
-            iframe.width = "560";
-            iframe.height = "315";
-            tooltipBox.appendChild(iframe);
-
-            hoverBox.forEach((obj, i) => {
-                obj.addEventListener("mouseenter", function() {
-                    const tl = gsap.timeline();
-                    tl
-                    .to(".cursor", {
-                        duration: 0.1,
-                        scale: 1.8,
-                        mixBlendMode: "difference",
-                    })
-                    .to(this, {
-                        duration: 0.3,
-                        letterSpacing: "3px",
-                    }, "<")
-                    .to(tooltipBox, {
-                        duration: 0.3,
-                        opacity: 1,
-                        display: "block",
-                    }, "<");
-                    // iframe.src = `https://www.youtube.com/embed/${embedUrl[i]}?rel=0&autoplay=1&mute=1&controls=0&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&color=white"`;
-                });
-
-                obj.addEventListener("mouseleave", function() {
-                    const tl = gsap.timeline();
-                    tl
-                    .to(".cursor", {
-                        duration: 0.1,
-                        scale: 1,
-                        mixBlendMode: "normal",
-                    })
-                    .to(this, {
-                        duration: 0.3,
-                        "letter-spacing": "0",
-                    }, "<")
-                    .to(tooltipBox, {
-                        duration: 0.3,
-                        opacity: 0,
-                        display: "none",
-                    }, "<");
-                });
-                
-                obj.addEventListener("mousemove", (e) => {
-                    const mouseX = e.clientX;
-                    const mouseY = e.clientY;
-
-                    tooltipBox.style.left = mouseX + "px";
-                    tooltipBox.style.top = mouseY - tooltipBox.clientHeight + "px";
-
-                    if (tooltipBox.offsetTop < 0) {
-                        tooltipBox.style.top = mouseY + "px";
-                    }
-                });
+    overEvent: (data) => {
+        if (!platformCheck()) return;
+    
+        const embedUrl = data.map(d => d.url);
+        const tooltipBox = document.querySelector(".tooltip_box");
+        const iframe = document.createElement("iframe");
+        const hoverBox = document.querySelectorAll(".work_list .list button");
+    
+        iframe.setAttribute("class", "video_tooltip");
+        iframe.setAttribute("name", "work");
+        iframe.setAttribute("frameborder", "0");
+        iframe.width = "560";
+        iframe.height = "315";
+        tooltipBox.appendChild(iframe);
+    
+        hoverBox.forEach((obj, i) => {
+            obj.addEventListener("mouseenter", function(e) {
+                const tl = gsap.timeline();
+                tl
+                .to(".cursor", {
+                    duration: 0.1,
+                    scale: 1.8,
+                    mixBlendMode: "difference",
+                })
+                .to(this, {
+                    duration: 0.3,
+                    letterSpacing: "3px",
+                }, "<")
+                .to(tooltipBox, {
+                    duration: 0.3,
+                    opacity: 1,
+                    display: "block",
+                }, "<");
+                // iframe.src = `https://www.youtube.com/embed/${embedUrl[i]}?rel=0&autoplay=1&mute=1&controls=0&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&color=white"`;
+                e.stopImmediatePropagation();
             });
-        },
+    
+            obj.addEventListener("mouseleave", function(e) {
+                const tl = gsap.timeline();
+                tl
+                .to(".cursor", {
+                    duration: 0.1,
+                    scale: 1,
+                    mixBlendMode: "normal",
+                })
+                .to(this, {
+                    duration: 0.3,
+                    "letter-spacing": "0",
+                }, "<")
+                .to(tooltipBox, {
+                    duration: 0.3,
+                    opacity: 0,
+                    display: "none",
+                }, "<");
+                e.stopImmediatePropagation();
+            });
+            
+            obj.addEventListener("mousemove", (e) => {
+                const mouseX = e.clientX;
+                const mouseY = e.clientY;
+    
+                tooltipBox.style.left = mouseX + "px";
+                tooltipBox.style.top = mouseY - tooltipBox.clientHeight + "px";
+    
+                if (tooltipBox.offsetTop < 0) tooltipBox.style.top = mouseY + "px";
+            });
+        });
     },
 
     clickEvent: () => {
