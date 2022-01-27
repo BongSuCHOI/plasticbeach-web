280942db655eebae06380881d514151733731916
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-01-27 19:28:04 +0900

    index.html 경로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./index.html",
+            template: "./src/index.html",
             filename: "./index.html",
         }),
         new MiniCssExtractPlugin({
