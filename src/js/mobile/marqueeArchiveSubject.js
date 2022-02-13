4ec0c83400892f275c4f2cf88bbda7cd9e9e28f5
BongSuCHOI <chlqhdtn98@gmail.com>
2022-02-14 01:47:03 +0900

    버그 수정

--- src/js/mobile/marqueeArchiveSubject.js
+++ src/js/mobile/marqueeArchiveSubject.js
@@ -19,63 +19,91 @@ function copySubject() {
 }
 
 // start
-function start(kill) {
+function start(k = "n") {
     const subjectElem = document.querySelectorAll(".archive_list .subject");
-    const speedRate = 25;
+    const speedRate = 20;
 
-    subjectElem.forEach((elem, i) => {
-        const container = elem.firstChild;
-        let distance = container.offsetWidth / 2;
-        const time = distance / speedRate;
-        const even = (i + 1) % 2 === 0;
+    if (k === "n") {
+        subjectElem.forEach((elem, i) => {
+            const container = elem.firstChild;
+            const distance = container.offsetWidth / 2;
+            const time = distance / speedRate;
+            const even = (i + 1) % 2 === 0;
+            gsap.killTweensOf(container);
+            gsap.set(container, { x: 0 });
 
-        const isEvenGSAP = gsap.to(container, {
-            duration: time,
-            repeat: -1,
-            x: distance,
-            ease: "none",
-            paused: true,
+            const isEvenGSAP = gsap.to(container, {
+                duration: time,
+                repeat: -1,
+                x: distance,
+                ease: "none",
+                paused: true,
+            });
+
+            const isOddGSAP = gsap.to(container, {
+                duration: time,
+                repeat: -1,
+                x: "-" + distance,
+                ease: "none",
+                paused: true,
+            });
+
+            if (even) {
+                elem.classList.add("even");
+                isEvenGSAP.play();
+            } else {
+                isOddGSAP.play();
+            }
         });
+    }
 
-        const isOddGSAP = gsap.to(container, {
-            duration: time,
-            repeat: -1,
-            x: "-" + distance,
-            ease: "none",
-            paused: true,
+    if (k === "k") {
+        subjectElem.forEach((elem, i) => {
+            const container = elem.firstChild;
+            const distance = container.offsetWidth / 2;
+            const time = distance / speedRate;
+            const even = (i + 1) % 2 === 0;
+            gsap.killTweensOf(container);
+            gsap.set(container, { x: 0 });
+
+            const isEvenGSAP = gsap.to(container, {
+                duration: time,
+                repeat: -1,
+                x: distance,
+                ease: "none",
+                paused: true,
+            });
+
+            const isOddGSAP = gsap.to(container, {
+                duration: time,
+                repeat: -1,
+                x: "-" + distance,
+                ease: "none",
+                paused: true,
+            });
+
+            if (even) {
+                isEvenGSAP.play();
+            } else {
+                isOddGSAP.play();
+            }
         });
-
-        if (kill === "kill") {
-            console.log("asdasdas");
-            isEvenGSAP.kill();
-            isOddGSAP.kill();
-        } else if (even) {
-            elem.classList.add("even");
-            isEvenGSAP.play();
-        } else {
-            isOddGSAP.play();
-        }
-
-        // const observer = new MutationObserver((mutations) => {
-        //     mutations.forEach((mutation) => {
-        //         let progress = isOddGSAP.totalProgress();
-        //         isOddGSAP.progress();
-        //         isOddGSAP.invalidate();
-        //         distance = container.offsetWidth;
-        //         isOddGSAP.totalProgress(progress);
-        //     });
-        // });
-        // observer.observe(container.parentElement, { attributeFilter: ["class"] });
-    });
+    }
 }
 
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-        start("kill");
+        const theme = mutation.target.getAttribute("color-theme");
+        if (theme === "blue") {
+            start("k");
+        } else {
+            start("n");
+        }
     });
 });
 observer.observe(document.documentElement, { attributeFilter: ["color-theme"] });
 
 // 버그 : 영/한 컨버트 시 변수 distance값이 유동적으로 계속 변경되어야 함
+// 위 버그는 잡았음 리팩토링 필요
 
 export default copySubject;
